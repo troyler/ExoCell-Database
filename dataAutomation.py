d545605b24279951cab9c7eba4420fe89e2497ea
troyler <tmreinert22@gmail.com>
2022-06-22 00:07:53 -0400

    Req Analysis

--- dataAutomation.py
+++ dataAutomation.py
@@ -14,57 +14,66 @@ path = r'C:\Users\trein\Desktop\MM210504 FCD files'
 filenames = glob.glob(path + '/*.fcd')
 print(filenames)
 
-def plotMechs(x,y):
-        plt.scatter(x,y,marker = ".")
-        plt.show()
-        plt.yticks(np.arange(0,1.1,0.1))
 
+def plotMechs(x, y):
+    plt.scatter(x, y, marker=".")
+    plt.show()
 
 
 def fileParser(filename):
-
     cleanData = []
     print(filename)
-    with open(filename,"r") as file:
+    with open(filename, "r") as file:
         for line in file:
             dataInfo = line.strip().split('\t')
             if len(dataInfo) > 10:
                 cleanData.append(dataInfo)
 
-    df = pd.DataFrame(cleanData[1:], columns = cleanData[0])
+    df = pd.DataFrame(cleanData[1:], columns=cleanData[0])
     print(df)
 
     if "OCV" in filename:
-        x= (df['Time (Sec)'])
-        x=np.array(list(map(float,x)))
-        y= df['E_Stack (V)']
-        y=np.array(list(map(float,y)))
-        plotMechs(x,y)
+        x = df['Time (Sec)']
+        x = np.array(list(map(float, x)))
+        y = df['E_Stack (V)']
+        y = np.array(list(map(float, y)))
+        plotMechs(x, y)
 
     elif "Cond" in filename:
-        x= (df['Time (Sec)'])
-        x=np.array(list(map(float,x)))
-        y= df['I (mA/cm²)']
-        y=np.array(list(map(float,y)))
-        plotMechs(x,y)
+        x = (df['Time (Sec)'])
+        x = np.array(list(map(float, x)))
+        y = df['I (mA/cm²)']
+        y = np.array(list(map(float, y)))
+        plt.yticks(np.arange(0, 1.1, 0.1))
+        plotMechs(x, y)
 
     elif "SC" in filename:
-        x= (df['I (mA/cm²)'])
-        y= df['E_Stack (V)']
+        x = (df['I (mA/cm²)'])
+        y = df['E_Stack (V)']
         z = df['Power (mW/cm²)']
-        x=np.array(list(map(float,x)))
-        y=np.array(list(map(float,y)))
-        z=np.array(list(map(float,z)))
-        plotMechs(x,y)
-        plotMechs(x,z)
+        x = np.array(list(map(float, x)))
+        y = np.array(list(map(float, y)))
+        z = np.array(list(map(float, z)))
+        plt.yticks(np.arange(0, 1.1, 0.1))
+        plotMechs(x, y)
+        plotMechs(x, z)
 
 
 for filename in filenames:
     fileParser(filename)
 
 
+# next steps
+# get all plots to show at once, not huge deal for end product
+# make figure consisting of all plots
+# get titles and axis titles displayed, use function
+# make plots interactive
+# make plots more readable, get maxima and minima on graph
 
-#next steps
-#get all plots to show at once, not huge deal for end product
-#make figure consisting of all plots
-#get titles and axis titles displayed, use function
+
+# need to set up website for form submission, allow for folders to be uploaded
+# set up backend database to store and organize files
+# ----> figure out what type of file is being generated, how can it be stored...
+# make system for accessing specific files by user, date, cell size, test type, run#
+# design sign-in system
+# get this thing hosted on a server
