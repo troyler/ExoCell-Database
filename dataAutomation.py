b961e4ce3575fcb1f24ca5be151f7a76426090f4
troyler <tmreinert22@gmail.com>
2022-06-26 21:08:55 -0400

    Automated Titles

--- dataAutomation.py
+++ dataAutomation.py
@@ -15,54 +15,70 @@ filenames = glob.glob(path + '/*.fcd')
 print(filenames)
 
 
-def plotMechs(x, y):
-    plt.scatter(x, y, marker=".")
+def plotMechs(x, y, xtitle, ytitle):
+     
+    plt.scatter(x,y, marker=".", s=1)
+
+    plt.xlabel(xtitle, fontsize=15)
+    plt.ylabel(ytitle, fontsize=15)
+    plt.title( f" {xtitle}   vs   {ytitle} ", fontsize=20,pad=10)
+
+    plt.grid(True)
     plt.show()
 
 
 def fileParser(filename):
     cleanData = []
+    zeroRow = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     print(filename)
     with open(filename, "r") as file:
         for line in file:
             dataInfo = line.strip().split('\t')
             if len(dataInfo) > 10:
-                cleanData.append(dataInfo)
+                    cleanData.append(dataInfo)
+    if cleanData[0][0] == "Time (Sec)" :  #mainly for QA 
+        df = pd.DataFrame(cleanData[1:], columns=cleanData[0])
+        df.loc[len(df)] = zeroRow
+        print(df)
 
-    df = pd.DataFrame(cleanData[1:], columns=cleanData[0])
-    print(df)
 
-    if "OCV" in filename:
-        x = df['Time (Sec)']
-        x = np.array(list(map(float, x)))
-        y = df['E_Stack (V)']
-        y = np.array(list(map(float, y)))
-        plotMechs(x, y)
+    
+        timeTitle = df.columns[0]
+        voltageTitle = df.columns[5]
+        currentDensityTitle = df.columns[2]
+        powerDensityTitle = df.columns[4]
+        
+        
 
-    elif "Cond" in filename:
-        x = (df['Time (Sec)'])
-        x = np.array(list(map(float, x)))
-        y = df['I (mA/cm²)']
-        y = np.array(list(map(float, y)))
-        plt.yticks(np.arange(0, 1.1, 0.1))
-        plotMechs(x, y)
+        if "OCV" in filename:
+            x = df['Time (Sec)']
+            x = np.array(list(map(float, x)))
+            y = df['E_Stack (V)']
+            y = np.array(list(map(float, y)))
+            plotMechs(x, y,timeTitle, voltageTitle)
 
-    elif "SC" in filename:
-        x = (df['I (mA/cm²)'])
-        y = df['E_Stack (V)']
-        z = df['Power (mW/cm²)']
-        x = np.array(list(map(float, x)))
-        y = np.array(list(map(float, y)))
-        z = np.array(list(map(float, z)))
-        plt.yticks(np.arange(0, 1.1, 0.1))
-        plotMechs(x, y)
-        plotMechs(x, z)
+        elif "Cond" in filename:
+            x = (df['Time (Sec)'])
+            x = np.array(list(map(float, x)))
+            y = df['I (mA/cm²)']
+            y = np.array(list(map(float, y)))
+            plotMechs(x, y,timeTitle,currentDensityTitle)
+
+        elif "SC" in filename:
+            x = (df['I (mA/cm²)'])
+            y = df['E_Stack (V)']
+            z = df['Power (mW/cm²)']
+            x = np.array(list(map(float, x)))
+            y = np.array(list(map(float, y)))
+            z = np.array(list(map(float, z)))
+            plt.yticks(np.arange(0, 1.1, 0.1))
+            plotMechs(x, y, currentDensityTitle,voltageTitle)
+            plotMechs(x, z, currentDensityTitle, powerDensityTitle)
+
 
 
 for filename in filenames:
     fileParser(filename)
-
-
 # next steps
 # get all plots to show at once, not huge deal for end product
 # make figure consisting of all plots
